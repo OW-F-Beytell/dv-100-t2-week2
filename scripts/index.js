a796673e62f2c9926d55c1260f18c7e301919547
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-02 12:22:57 +0200

    End of practical class

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+console.log("Hello world!");
+
+var firstName = "Freddy";
+let lastName = "Beytell";
+const subject = "Development";
+const age = 21;
+
+console.log(firstName);
+console.log(lastName);
+console.log(subject);
+console.log(age);
+
+let greeter = "Welcome " + firstName + " " + lastName + ", your subject choice is " + subject + ". You are " + age + " years old.";
+console.log(greeter); 
+
+let susan = 12;
+let timmy = 20;
+let josh = 14;
+let win = false;
+
+let teamST = susan + timmy;
+console.log(teamST);
+let victory = josh / 2;
+console.log(victory);
+let answer = teamST + victory;
+console.log(answer);
+console.log(win);
